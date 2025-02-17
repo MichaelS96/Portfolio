4a586e52c5b6917b69a01f8ed81724cb9b1a1b04
MichaelS96 <m.schwittay96@web.de>
2025-02-17 13:25:17 +0100

    header and footer component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboveTheFoldComponent } from "./main-content/above-the-fold/above-the-fold.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
