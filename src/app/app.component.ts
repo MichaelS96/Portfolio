1e9eb555ff18f8ac7dc07cc6ba267b941be20213
MichaelS96 <m.schwittay96@web.de>
2025-02-17 16:40:35 +0100

    check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from "./main-content/above-the-fold/above-the-fold.component";
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { AboutMeComponent } from "./main-content/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
