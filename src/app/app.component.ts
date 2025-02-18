92601cef2c183efd4ea8325a84ad24dde83ca965
MichaelS96 <m.schwittay96@web.de>
2025-02-18 14:22:43 +0100

    skills added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AboveTheFoldComponent } from "./main-content/above-the-fold/above-the-f
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { AboutMeComponent } from "./main-content/about-me/about-me.component";
 import { SkillsComponent } from "./main-content/skills/skills.component";
+import { PortfolioComponent } from "./main-content/portfolio/portfolio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills: { image: string, name: string }[] = [
+    {
+      image: './assets/img/skills/html.png',
+      name: 'HTML'
+    },
+    {
+      image: './assets/img/skills/css.png',
+      name: 'CSS'
+    },
+    {
+      image: './assets/img/skills/javascript.png',
+      name: 'JavaScript'
+    },
+    {
+      image: './assets/img/skills/angular.png',
+      name: 'Angular'
+    },
+    {
+      image: './assets/img/skills/materialdesign.png',
+      name: 'Material Design'
+    },
+    {
+      image: './assets/img/skills/learning.png',
+      name: 'Continually Learning'
+    },
+  ]
 }
