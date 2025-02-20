207987eaeaa53ec284beb5e7e65b9d8ceb1c0353
MichaelS96 <m.schwittay96@web.de>
2025-02-20 18:19:43 +0100

    feedback section finished

--- src/app/main-content/portfolio/feedback/feedback.component.ts
+++ src/app/main-content/portfolio/feedback/feedback.component.ts
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
+  currentFeedbackIndex: number = 0;
 
+  feedbacks: { name: string; feedback: string; role: string; image: string }[] = [
+    {
+      name: 'Albert',
+      feedback: 'Starke arbeit macht spass mit Michael zu arbeiten',
+      role: 'Team Partner',
+      image: './assets/img/about-me/pb123.png',
+    },
+    {
+      name: 'Rainer',
+      feedback: 'Langweiliger Typ',
+      role: 'Team Partner',
+      image: '',
+    },
+  ];
+
+  get currentFeedback() {
+    return this.feedbacks[this.currentFeedbackIndex];
+  }
+
+  showNextFeedback() {
+    this.currentFeedbackIndex = (this.currentFeedbackIndex + 1) % this.feedbacks.length;
+  }
+
+  showPreviousFeedback() {
+    this.currentFeedbackIndex = (this.currentFeedbackIndex - 1 + this.feedbacks.length) % this.feedbacks.length;
+  }
+
+  updateFeedbackIndex(index: number) {
+    this.currentFeedbackIndex = index;
+  }
 }
