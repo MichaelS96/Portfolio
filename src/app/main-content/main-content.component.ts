ee84407b99649e4b874b350c82105e7b170a4197
MichaelS96 <m.schwittay96@web.de>
2025-02-20 15:27:45 +0100

    feedback added to site

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,6 +5,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AppComponent } from '../app.component';
+import { FeedbackComponent } from './portfolio/feedback/feedback.component';
 
 
 @Component({
@@ -16,6 +17,7 @@ import { AppComponent } from '../app.component';
     AboutMeComponent,
     SkillsComponent,
     PortfolioComponent,
+    FeedbackComponent
 ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
