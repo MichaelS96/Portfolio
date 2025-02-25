b3dcc7aff3ad5dc2a842c5e11cd185f5776618c4
MichaelS96 <m.schwittay96@web.de>
2025-02-25 10:36:08 +0100

    impressum finished

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
