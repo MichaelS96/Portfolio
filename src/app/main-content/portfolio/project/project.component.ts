97f497007f290d7c72fd9042bab7a3f56a1112d4
MichaelS96 <m.schwittay96@web.de>
2025-02-26 11:15:29 +0100

    portfolio array de-en added

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -4,7 +4,7 @@ import { TranslatePipe } from "@ngx-translate/core";
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
@@ -15,7 +15,7 @@ export class ProjectComponent {
       name: 'Pokedex',
       image: './assets/img/projects/pokedex.png',
       type: 'JavaScript | HTML | CSS | Api',
-      description: 'Auf Grundlage der PokéAPI, einer benutzerfreundlichen Bibliothek, die detaillierte Informationen zu Pokémon erfasst und bereitstellt.',
+      description: 'portfolio.pokedex',
       link: {
         liveTest: '',
         github: 'https://github.com/MichaelS96/Pokedex',
@@ -25,7 +25,7 @@ export class ProjectComponent {
       name: 'El Pollo Loco',
       image: './assets/img/projects/polloloco.png',
       type: 'JavaScript | HTML | CSS',
-      description: 'Springen, rennen und werfen ein spannendes Spiel mit einem objektorientierten Konzept. Begleite Pepe auf seinem Abenteuer, sammle Münzen und scharfe Tabasco-Salsa, um dich der verrückten Henne im ultimativen Duell zu stellen!',
+      description: 'portfolio.polloloco',
       link: {
         liveTest: '/',
         github: 'https://github.com/MichaelS96/El-Pollo-Loco',
