2a2440d72efa18cece11edba303e554bb46a58f5
MichaelS96 <m.schwittay96@web.de>
2025-02-28 14:28:49 +0100

    added a few translations

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { TranslatePipe, TranslateDirective } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,TranslatePipe],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
